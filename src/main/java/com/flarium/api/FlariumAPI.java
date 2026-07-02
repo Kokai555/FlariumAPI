d00b99d98674187ee68bbe197cd912f5c41004cb
Kokai Milan <kokaimilan708@gmail.com>
2026-07-02 10:18:34 +0200

    Updated FlariumAPI.java

--- src/main/java/com/flarium/api/FlariumAPI.java
+++ src/main/java/com/flarium/api/FlariumAPI.java
@@ -1,16 +1,42 @@
 package com.flarium.api;
 
+import com.flarium.api.data.PDCManager;
+import com.flarium.api.hologram.HologramListener;
+import com.flarium.api.hologram.HologramManager;
+import com.flarium.api.scheduler.Scheduler;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class FlariumAPI extends JavaPlugin {
+public class FlariumAPI extends JavaPlugin {
+
+    private static FlariumAPI instance;
+
+    private Scheduler scheduler;
+    private PDCManager pdcManager;
+    private HologramManager hologramManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+
+        this.scheduler = new Scheduler(this);
+        this.pdcManager = new PDCManager(this);
+        this.hologramManager = new HologramManager(this, scheduler, pdcManager);
+
+        getServer().getPluginManager().registerEvents(new HologramListener(hologramManager, pdcManager, scheduler), this);
+
+        getLogger().info("FlariumAPI enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (hologramManager != null) hologramManager.shutdown();
+        if (scheduler != null) scheduler.shutdown();
+        getLogger().info("FlariumAPI disabled!");
     }
+
+    public static FlariumAPI getInstance() { return instance; }
+
+    public Scheduler getScheduler() { return scheduler; }
+    public PDCManager getPdcManager() { return pdcManager; }
+    public HologramManager getHologramManager() { return hologramManager; }
 }
